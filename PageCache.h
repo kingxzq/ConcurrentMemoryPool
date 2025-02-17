2417a9c609721a1ca2a281f1764d1d1bce7e6d67
Xiaopihai <62838713+kingxzq@users.noreply.github.com>
2025-02-17 20:48:17 +0800

    Add files via upload

---
+++ PageCache.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Common.h"
+#include "ObjectPool.h"
+#include "PageMap.h"
+
+class PageCache
+{
+public:
+	static PageCache* GetInstance()
+	{
+		return &_sInst;
+	}
+
+	// 获取从对象到span的映射
+	Span* MapObjectToSpan(void* obj);
+
+	// 释放空闲span回到Pagecache，并合并相邻的span
+	void ReleaseSpanToPageCache(Span* span);
+
+	// 获取一个K页的span
+	Span* NewSpan(size_t k);
+
+	std::mutex _pageMtx;
+private:
+	SpanList _spanLists[NPAGES];
+	ObjectPool<Span> _spanPool;
+
+	TCMalloc_PageMap1<32 - PAGE_SHIFT> _idSpanMap;
+
+	PageCache()
+	{}
+	PageCache(const PageCache&) = delete;
+
+
+	static PageCache _sInst;
+};
